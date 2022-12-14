8402ce9fa9f00eb1e445a3815e18154480819a1c
Atomute <73423300+Atomute@users.noreply.github.com>
2022-12-14 20:20:01 +0700

    Create Ex.java

---
+++ Ex.java
@@ -0,0 +1,14 @@
+public class Ex{
+    Integer[] myString = {0,-1,2,-3,1,-2};
+    public static void main(String[] args){
+        for (int i=0; i<6 ; i++){
+            
+        }
+
+        System.out.println("Hello World");
+    }
+}
+
+class Firstclass {
+
+}
