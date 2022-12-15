6241bd40b4217c33e3c754c09d68778c7b727545
Atomute <73423300+Atomute@users.noreply.github.com>
2022-12-15 19:32:17 +0700

    ex6 update

--- w1/Ex6.java
+++ w1/Ex6.java
@@ -1,50 +1,45 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
-import java.util.Arrays;
-
 public class Ex6 {
-    static String[] readfile(){
-        String[] fromtxt = {};
+    static int findtenROW(int[][] matrix){
+        int a = 0;
         int counter = 0;
-        try {
-            File myObj = new File("forEX6.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                fromtxt = Arrays.copyOf(fromtxt, fromtxt.length+1);
-                fromtxt[counter] = data;
-                System.out.println(data);
-                counter += 1;
-            }
-            myReader.close();
-          } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-          }
-        return fromtxt;
-    }
-    static int[][] createMatrix(String[] arr,int start){
-        int matrixdimension = Integer.parseInt(arr[start]);
-        int[][] myMatrix = new int[matrixdimension][matrixdimension];
-
-        for (int row=0;row<matrixdimension;row++){
-            String[] line = arr[start+row].split(" ",matrixdimension);
-            for (int col=0;col<matrixdimension;col++){
-                myMatrix[row][col] = Integer.parseInt(line[col]);
+        for (int row=0;row<matrix.length;row++){
+            a = 0;
+            for (int col=0;col<matrix.length;col++){
+                // System.out.println(matrix[row][col]);
+                a += matrix[row][col];
+                if (a == 10){
+                    counter += 1;
+                    break;
+                }else if(a > 10){
+                    break;
+                }  
             }
         }
-        return myMatrix;
+        return counter;
+    }
+
+    static int findtencol(int[][] matrix){
+        int a = 0;
+        int counter = 0;
+        for (int col=0;col<matrix.length;col++){
+            a = 0;
+            for (int row=0;row<matrix.length;row++){
+                // System.out.println(matrix[row][col]);
+                a += matrix[row][col];
+                if (a == 10){
+                    counter += 1;
+                    break;
+                }else if(a > 10){
+                    break;
+                }  
+            }
+        }
+        return counter;
     }
     public static void main(String[] args){
-        String[] mytxt = readfile();
-        int setofdata = Integer.parseInt(mytxt[0]);
-        int counter = 0;
-        int[][] myMatrix = {};
-        
-        for (int i=0;i<setofdata;i++){
-            myMatrix = createMatrix(mytxt, 1);
-        }
-        System.out.print(Arrays.toString(myMatrix));
+        int[][] myMatrix = {{2,2,2,2,2},{2,2,2,2,2},{2,2,2,2,2},{2,2,2,2,2},{2,2,2,2,2}};
+        int rowcount = findtenROW(myMatrix);
+        int colcount = findtencol(myMatrix);
+        System.out.print(rowcount+colcount);
     }
 }
--- w1/test.java
+++ w1/test.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args){
+        int[][] myMatrix = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
         String arr = "1 2 3 4";
-        System.out.println(Arrays.toString(arr.split(" ",4)));
+        System.out.println(myMatrix.length);
     }
 }
