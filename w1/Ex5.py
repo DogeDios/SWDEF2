d80914b301c80c4d631b8af7a6261a1627641919
Atomute <73423300+Atomute@users.noreply.github.com>
2022-12-17 20:40:51 +0700

    edit Ex5.py

--- w1/Ex5.py
+++ w1/Ex5.py
@@ -11,8 +11,7 @@ def threeSum(arr):
                 if a in arr and arr[i] != arr[j] and arr[i]<0 and arr[j]>=0 and a > arr[i] and a < arr[j]:
                     ans.append("[ {}, {}, {}]".format(arr[i],arr[j],a))
     return ans
-arr = [-3,-2,-1,0,1,2,3,5]
-arr.sort
+arr = [-1,-2,-9,-5,9,7,5,6]
 
 print(threeSum(arr))
 
